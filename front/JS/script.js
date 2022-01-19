5b555a1130d6f52e7e40c64c8db6dfb317f6567f
Nicolas-Maillols <nicomaillols@gmail.com>
2022-01-19 20:20:36 +0100

    auto-load for getAllCountries

--- front/JS/script.js
+++ front/JS/script.js
@@ -1,41 +1,81 @@
-$("#btnShowData").click(function getAllCountries() {
+$(() => {
 
-    $.ajax({
 
-        url: "https://restcountries.com/v3.1/all",
-
-        success: function (data) {
-
-            data.map((country) => {
-
-                $("#list").append(
-                    `<h3>${country.name.common}</h3>`
-                );
-
-                $("#list").append(
-                    `<li>Capitale : ${country.capital}</li>`
-                );
-
-                $("#list").append(
-                    `<li>Continent : ${country.region}</li>`
-                );
-
-                for(language in country.languages) {
+    $(document).ready(function getAllCountries() {
+    
+        $.ajax({
+    
+            url: "https://restcountries.com/v3.1/all",
+    
+            success: function (data) {
+    
+                data.map((country) => {
+    
                     $("#list").append(
-                        `<li>Langue : ${country.languages[language]}</li>`
+                        `<h3>${country.name.common}</h3>`,
+                        `<li>Capitale : ${country.capital}</li>`,
+                        `<li>Continent : ${country.region}</li>`,
                     );
-                }
-
-                for(currencie in country.currencies) {
+    
+                    for (language in country.languages) {
+                        $("#list").append(
+                            `<li>Langue : ${country.languages[language]}</li>`
+                        );
+                    }
+    
+                    for (currencie in country.currencies) {
+                        $("#list").append(
+                            `<li>Money : ${country.currencies[currencie].name}</li>`
+                        );
+    
+                    }
+    
+                })
+    
+            }
+        })
+    });
+    
+    
+    
+    $("#btnShowData").click(function getMyCountry() {
+    
+        let textArea = $("#request");
+        textArea = textArea.val();
+    
+        $.ajax({
+    
+            url: `https://restcountries.com/v3.1/name/${textArea}`,
+    
+            success: function (data) {
+    
+                data.map((country) => {
+    
                     $("#list").append(
-                        `<li>Money : ${country.currencies[currencie].name}</li>`
+                        `<h3>${country.name.common}</h3>`,
+                        `<li>Capitale : ${country.capital}</li>`,
+                        `<li>Continent : ${country.region}</li>`,
                     );
+    
+                    for (language in country.languages) {
+                        $("#list").append(
+                            `<li>Langue : ${country.languages[language]}</li>`
+                        );
+                    }
+    
+                    for (currencie in country.currencies) {
+                        $("#list").append(
+                            `<li>Money : ${country.currencies[currencie].name}</li>`
+                        );
+    
+                    }
+    
+                })
+    
+            }
+        })
+    });
 
-                }
 
-            })
-
-        }
-    })
-});
+})    
 
