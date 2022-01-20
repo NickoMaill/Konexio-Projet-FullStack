3d0e9f58a6b6c3155f1766463042d84e7a2e76e5
Nicko <nickos@iMac.local>
2022-01-20 09:00:35 +0100

    insert money&symbols in data

--- front/JS/script.js
+++ front/JS/script.js
@@ -1,4 +1,4 @@
-const list = document.querySelector("#list");
+const list = document.querySelector("#countryList");
 const btn = document.querySelector("#btnShowData");
 const input = document.querySelector("#req").value;
 const url = `https://restcountries.com/v3.1/all`;
@@ -18,36 +18,51 @@ function getAllCountries() {
 
                 let countryName = `${country.name.common}`;
                 let liCountry = document.createElement("li");
-                let ulCountry = document.createElement("ul");
+                liCountry.className = "name";
+                let ul = document.createElement("ul");
+                ul.className = "countryData";
                 liCountry.textContent = countryName;
 
                 let capital = `Capital : ${country.capital}`;
-                let liCapital = document.createElement("li");;
+                let liCapital = document.createElement("li");
+                liCapital.className = "data";
                 liCapital.textContent = capital;
 
                 let region = `Region : ${country.region}`;
                 let liRegion = document.createElement("li");
+                liRegion.className = "data";
                 liRegion.textContent = region;
 
                 let subRegion = `Subregion : ${country.subregion}`;
                 let liSubRegion = document.createElement("li");
+                liSubRegion.className = "data";
                 liSubRegion.textContent = subRegion;
 
-                // let language = `Langue : ${country.languages.language}`;
-                // let liLanguage = document.createElement("li");
-                // liLanguage.textContent = language;
-
-                // console.log(liLanguage);
-
+                // let money = `Money : ${country.currencies.name} ${country.currencies.symbol}`;
+                // let liMoney = document.createElement("li");
+                // liMoney.className = "data";
+                // liMoney.textContent = money;
+                
+                for (currency in country.currencies) {
+                    
+                    let money = `Money : ${country.currencies[currency].name} "${country.currencies[currency].symbol}"  `
+                    let liMoney = document.createElement("li");
+                    liMoney.className = "data";
+                    liMoney.textContent = money;
+                    ul.insertAdjacentElement("beforeend", liMoney)
+                    //console.log(liMoney);
+                }
+                
+                
                 list
-                    .appendChild(liCountry)
-                    .appendChild(ulCountry)
-                    .appendChild(liCapital)
-                    .insertAdjacentElement("afterend", liRegion)
-                    .insertAdjacentElement("afterend", liSubRegion)
-                // .insertAdjacentElement("afterend", liLanguage)
-
-
+                .appendChild(liCountry)
+                .appendChild(ul)
+                .appendChild(liCapital)
+                .insertAdjacentElement("afterend", liRegion)
+                .insertAdjacentElement("afterend", liSubRegion)
+                // .insertAdjacentElement("afterend", liMoney);
+                
+                
             });
 
 
