e609bb5f01c7f7d363c6d77ceaad044729f7d8e8
Nicko <nickos@iMac.local>
2022-01-20 09:08:11 +0100

    insert languages in data

--- front/JS/script.js
+++ front/JS/script.js
@@ -23,17 +23,17 @@ function getAllCountries() {
                 ul.className = "countryData";
                 liCountry.textContent = countryName;
 
-                let capital = `Capital : ${country.capital}`;
+                let capital = `Capitale : ${country.capital}`;
                 let liCapital = document.createElement("li");
                 liCapital.className = "data";
                 liCapital.textContent = capital;
 
-                let region = `Region : ${country.region}`;
+                let region = `Continent : ${country.region}`;
                 let liRegion = document.createElement("li");
                 liRegion.className = "data";
                 liRegion.textContent = region;
 
-                let subRegion = `Subregion : ${country.subregion}`;
+                let subRegion = `Sous-Continent : ${country.subregion}`;
                 let liSubRegion = document.createElement("li");
                 liSubRegion.className = "data";
                 liSubRegion.textContent = subRegion;
@@ -43,15 +43,6 @@ function getAllCountries() {
                 // liMoney.className = "data";
                 // liMoney.textContent = money;
                 
-                for (currency in country.currencies) {
-                    
-                    let money = `Money : ${country.currencies[currency].name} "${country.currencies[currency].symbol}"  `
-                    let liMoney = document.createElement("li");
-                    liMoney.className = "data";
-                    liMoney.textContent = money;
-                    ul.insertAdjacentElement("beforeend", liMoney)
-                    //console.log(liMoney);
-                }
                 
                 
                 list
@@ -60,8 +51,27 @@ function getAllCountries() {
                 .appendChild(liCapital)
                 .insertAdjacentElement("afterend", liRegion)
                 .insertAdjacentElement("afterend", liSubRegion)
-                // .insertAdjacentElement("afterend", liMoney);
+
+
+                for (language in country.languages) {
+                    
+                    let languages = `Langue : ${country.languages[language]}`
+                    let liLanguages = document.createElement("li");
+                    liLanguages.className = "data";
+                    liLanguages.textContent = languages;
+                    ul.insertAdjacentElement("beforeend", liLanguages)
+                    //console.log(liMoney);
+                }
                 
+                for (currency in country.currencies) {
+                    
+                    let money = `Monaie : ${country.currencies[currency].name} "${country.currencies[currency].symbol}"  `
+                    let liMoney = document.createElement("li");
+                    liMoney.className = "data";
+                    liMoney.textContent = money;
+                    ul.insertAdjacentElement("beforeend", liMoney)
+                    //console.log(liMoney);
+                }
                 
             });
 
