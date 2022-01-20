388099dbb517edec01b8effcb3b60ab752fc14b5
Nicko <nickos@iMac.local>
2022-01-20 11:32:25 +0100

    function remove pt2

--- front/JS/script.js
+++ front/JS/script.js
@@ -7,6 +7,81 @@ const url2 = "https://restcountries.com/v3.1/name/france"
 
 //console.log(input)
 
+function getMyCountries() {
+
+    fetch(url2)
+        .then(res => res.json())
+        .then(data => {
+            //console.log(data);
+
+            data.map((country) => {
+
+                
+                let countryName = `${country.name.common}`;
+                let liCountry = document.createElement("li");
+                liCountry.className = "name";
+                let ul = document.createElement("ul");
+                ul.className = "countryData";
+                liCountry.textContent = countryName;
+                
+                let capital = `Capitale : ${country.capital}`;
+                let liCapital = document.createElement("li");
+                liCapital.className = "data";
+                liCapital.textContent = capital;
+                
+                let region = `Continent : ${country.region}`;
+                let liRegion = document.createElement("li");
+                liRegion.className = "data";
+                liRegion.textContent = region;
+                
+                let subRegion = `Sous-Continent : ${country.subregion}`;
+                let liSubRegion = document.createElement("li");
+                liSubRegion.className = "data";
+                liSubRegion.textContent = subRegion;
+                
+                // list.parentNode.removeChild(liCountry);
+                
+                
+                list
+                .appendChild(liCountry)
+                .appendChild(ul)
+                .appendChild(liCapital)
+                .insertAdjacentElement("afterend", liRegion)
+                .insertAdjacentElement("afterend", liSubRegion);
+                
+                document.querySelector("#name").style.display = "none";
+
+                for (lang in country.languages) {
+
+                    let languages = `Langue : ${country.languages[lang]}`
+                    let liLanguages = document.createElement("li");
+                    liLanguages.className = "data";
+                    liLanguages.textContent = languages;
+                    ul.insertAdjacentElement("beforeend", liLanguages)
+                    //console.log(country.languages);
+                    // const proLang = Object.getOwnPropertySymbols(languages)
+                    // console.log(proLang.length);
+                }
+
+                for (currency in country.currencies) {
+
+                    let money = `Monaie : ${country.currencies[currency].name} "${country.currencies[currency].symbol}"`
+                    let liMoney = document.createElement("li");
+                    liMoney.className = "data";
+                    liMoney.textContent = money;
+                    ul.insertAdjacentElement("beforeend", liMoney)
+                    //console.log(liMoney);
+                }
+
+            });
+
+        });
+
+};
+
+
+btn.addEventListener("click", (getMyCountries));
+
 function getAllCountries() {
 
     fetch(url)
@@ -67,80 +142,10 @@ function getAllCountries() {
                     //console.log(liMoney);
                 }
 
-
-                btn.addEventListener("click", () => {
-                    list.removeChild(list.children)
-                });
             });
 
         });
 
 };
 
-function getMyCountries() {
 
-    fetch(url2)
-        .then(res => res.json())
-        .then(data => {
-            //console.log(data);
-
-            data.map((country) => {
-
-                let countryName = `${country.name.common}`;
-                let liCountry = document.createElement("li");
-                liCountry.className = "name";
-                let ul = document.createElement("ul");
-                ul.className = "countryData";
-                liCountry.textContent = countryName;
-
-                let capital = `Capitale : ${country.capital}`;
-                let liCapital = document.createElement("li");
-                liCapital.className = "data";
-                liCapital.textContent = capital;
-
-                let region = `Continent : ${country.region}`;
-                let liRegion = document.createElement("li");
-                liRegion.className = "data";
-                liRegion.textContent = region;
-
-                let subRegion = `Sous-Continent : ${country.subregion}`;
-                let liSubRegion = document.createElement("li");
-                liSubRegion.className = "data";
-                liSubRegion.textContent = subRegion;
-
-                list
-                    .appendChild(liCountry)
-                    .appendChild(ul)
-                    .appendChild(liCapital)
-                    .insertAdjacentElement("afterend", liRegion)
-                    .insertAdjacentElement("afterend", liSubRegion)
-
-                for (lang in country.languages) {
-
-                    let languages = `Langue : ${country.languages[lang]}`
-                    let liLanguages = document.createElement("li");
-                    liLanguages.className = "data";
-                    liLanguages.textContent = languages;
-                    ul.insertAdjacentElement("beforeend", liLanguages)
-                    //console.log(country.languages);
-                    // const proLang = Object.getOwnPropertySymbols(languages)
-                    // console.log(proLang.length);
-                }
-
-                for (currency in country.currencies) {
-
-                    let money = `Monaie : ${country.currencies[currency].name} "${country.currencies[currency].symbol}"`
-                    let liMoney = document.createElement("li");
-                    liMoney.className = "data";
-                    liMoney.textContent = money;
-                    ul.insertAdjacentElement("beforeend", liMoney)
-                    //console.log(liMoney);
-                }
-
-            });
-
-        });
-
-};
-
-//btn.addEventListener("click", (getMyCountries));
