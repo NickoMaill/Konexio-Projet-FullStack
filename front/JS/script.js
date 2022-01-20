6bd12d0ed9f8e7853de7b870f5997ff1ecb80d4f
Nicko <nickos@iMac.local>
2022-01-20 17:19:05 +0100

    all functions are done ready for bonus \m/

--- front/JS/script.js
+++ front/JS/script.js
@@ -10,69 +10,69 @@ const url = "https://restcountries.com/v3.1/all";
 function getAllCountries() {
 
     fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        
-        data.map((country) => {
-            
-            let countryName = `${country.name.common}`;
-            let liCountry = document.createElement("li");
-            liCountry.className = "name";
-            let ul = document.createElement("ul");
-            ul.className = "countryData";
-            liCountry.textContent = countryName;
-            
-            let capital = `Capitale : ${country.capital}`;
-            let liCapital = document.createElement("li");
-            liCapital.className = "data";
-            liCapital.textContent = capital;
-            
-            let region = `Continent : ${country.region}`;
-            let liRegion = document.createElement("li");
-            liRegion.className = "data";
-            liRegion.textContent = region;
-            
-            let subRegion = `Sous-Continent : ${country.subregion}`;
-            let liSubRegion = document.createElement("li");
-            liSubRegion.className = "data";
-            liSubRegion.textContent = subRegion;
-            
-            list
-            .appendChild(liCountry)
-            .appendChild(ul)
-            .appendChild(liCapital)
-            .insertAdjacentElement("afterend", liRegion)
-            .insertAdjacentElement("afterend", liSubRegion)
-            
-            for (lang in country.languages) {
-                
-                let languages = `Langue : ${country.languages[lang]}`
-                let liLanguages = document.createElement("li");
-                liLanguages.className = "data";
-                liLanguages.textContent = languages;
-                ul.insertAdjacentElement("beforeend", liLanguages)
-            }
-            
-            for (currency in country.currencies) {
-                
-                let money = `Monaie : ${country.currencies[currency].name} "${country.currencies[currency].symbol}"`
-                let liMoney = document.createElement("li");
-                liMoney.className = "data";
-                liMoney.textContent = money;
-                ul.insertAdjacentElement("beforeend", liMoney)
-            }
-            
+        .then(res => res.json())
+        .then(data => {
+
+            data.map((country) => {
+
+                let countryName = `${country.name.common}`;
+                let liCountry = document.createElement("li");
+                liCountry.className = "name";
+                let ul = document.createElement("ul");
+                ul.className = "countryData";
+                liCountry.textContent = countryName;
+
+                let capital = `Capitale : ${country.capital}`;
+                let liCapital = document.createElement("li");
+                liCapital.className = "data";
+                liCapital.textContent = capital;
+
+                let region = `Continent : ${country.region}`;
+                let liRegion = document.createElement("li");
+                liRegion.className = "data";
+                liRegion.textContent = region;
+
+                let subRegion = `Sous-Continent : ${country.subregion}`;
+                let liSubRegion = document.createElement("li");
+                liSubRegion.className = "data";
+                liSubRegion.textContent = subRegion;
+
+                list
+                    .appendChild(liCountry)
+                    .appendChild(ul)
+                    .appendChild(liCapital)
+                    .insertAdjacentElement("afterend", liRegion)
+                    .insertAdjacentElement("afterend", liSubRegion)
+
+                for (lang in country.languages) {
+
+                    let languages = `Langue : ${country.languages[lang]}`
+                    let liLanguages = document.createElement("li");
+                    liLanguages.className = "data";
+                    liLanguages.textContent = languages;
+                    ul.insertAdjacentElement("beforeend", liLanguages)
+                }
+
+                for (currency in country.currencies) {
+
+                    let money = `Monaie : ${country.currencies[currency].name} "${country.currencies[currency].symbol}"`
+                    let liMoney = document.createElement("li");
+                    liMoney.className = "data";
+                    liMoney.textContent = money;
+                    ul.insertAdjacentElement("beforeend", liMoney)
+                }
+
+            });
+
         });
-        
-    });
-    
+
 };
 
-btn.addEventListener("click", () => {
-    let regionSelect = document.querySelector("#subRegionMenu")
-    regionSelect = regionSelect.value
-    console.log(regionSelect);
-})
+// btn.addEventListener("click", () => {
+//     let regionSelect = document.querySelector("#subRegionMenu")
+//     regionSelect = regionSelect.value
+//     console.log(regionSelect);
+// })
 
 //Function to get only one country data by radio select, by country name or Capital city
 
@@ -90,91 +90,108 @@ function getMyCountries() {
     let checkCountry = document.querySelector("#countryNameRadio");
     let checkCapital = document.querySelector("#capitalRadio");
     let checkRegion = document.querySelector("#regionRadio");
-    let checkSubRegion = document.querySelector("#subRegionRadio")
+    let checkSubRegion = document.querySelector("#subRegionRadio");
 
     const urlCountry = `https://restcountries.com/v3.1/name/${input}`;
     const urlCapital = `https://restcountries.com/v3.1/capital/${input}`;
-    const ulrRegion =  `https://restcountries.com/v3.1/region/${regionSelect}`
-    const ulrSubRegion =  `https://restcountries.com/v3.1/region/${subRegionSelect}`
+    const ulrRegion = `https://restcountries.com/v3.1/region/${regionSelect}`
+    const ulrSubRegion = `https://restcountries.com/v3.1/region/${subRegionSelect}`
     let url2 = undefined;
 
     //condition for input radio Country or Capital city
-
+    
     if (checkCountry.checked === true) {
-        url2 = urlCountry ;
+
+        while (list.firstChild) {
+            list.firstChild.remove()
+        };
+
+        url2 = urlCountry;
 
     } else if (checkCapital.checked === true) {
+
+        while (list.firstChild) {
+            list.firstChild.remove()
+        };
+
         url2 = urlCapital;
 
     } else if (checkRegion.checked === true) {
+
+        while (list.firstChild) {
+            list.firstChild.remove()
+        };
+
         url2 = ulrRegion;
 
-    } else if (checkSubRegion.checked === true){
+    } else if (checkSubRegion.checked === true) {
+
+        while (list.firstChild) {
+            list.firstChild.remove()
+        };
+
         url2 = ulrSubRegion;
     }
 
     fetch(url2)
-    .then(res => res.json())
-    .then(data => {
-        //console.log(data);
-        
-        data.map((country) => {
-            
-            let countryName = `${country.name.common}`;
-            let liCountry = document.createElement("li");
-            liCountry.className = "name";
-            let ul = document.createElement("ul");
-            ul.className = "countryData";
-            liCountry.textContent = countryName;
-            
-            let capital = `Capitale : ${country.capital}`;
-            let liCapital = document.createElement("li");
-            liCapital.className = "data";
-            liCapital.textContent = capital;
-            
-            let region = `Continent : ${country.region}`;
-            let liRegion = document.createElement("li");
-            liRegion.className = "data";
-            liRegion.textContent = region;
-            
-            let subRegion = `Sous-Continent : ${country.subregion}`;
-            let liSubRegion = document.createElement("li");
-            liSubRegion.className = "data";
-            liSubRegion.textContent = subRegion;
-            
-            while (list.firstChild) {
-                list.firstChild.remove()
-            };
-            
-            list
-            .appendChild(liCountry)
-            .appendChild(ul)
-            .appendChild(liCapital)
-            .insertAdjacentElement("afterend", liRegion)
-            .insertAdjacentElement("afterend", liSubRegion);
-            
-            for (lang in country.languages) {
+        .then(res => res.json())
+        .then(data => {
+            //console.log(data);
+
+            data.map((country) => {
+
+                let countryName = `${country.name.common}`;
+                let liCountry = document.createElement("li");
+                liCountry.className = "name";
+                let ul = document.createElement("ul");
+                ul.className = "countryData";
+                liCountry.textContent = countryName;
+
+                let capital = `Capitale : ${country.capital}`;
+                let liCapital = document.createElement("li");
+                liCapital.className = "data";
+                liCapital.textContent = capital;
+
+                let region = `Continent : ${country.region}`;
+                let liRegion = document.createElement("li");
+                liRegion.className = "data";
+                liRegion.textContent = region;
+
+                let subRegion = `Sous-Continent : ${country.subregion}`;
+                let liSubRegion = document.createElement("li");
+                liSubRegion.className = "data";
+                liSubRegion.textContent = subRegion;
                 
-                let languages = `Langue : ${country.languages[lang]}`
-                let liLanguages = document.createElement("li");
-                liLanguages.className = "data";
-                liLanguages.textContent = languages;
-                ul.insertAdjacentElement("beforeend", liLanguages)
-            };
-            
-            for (currency in country.currencies) {
-                
-                let money = `Monaie : ${country.currencies[currency].name} "${country.currencies[currency].symbol}"`
-                let liMoney = document.createElement("li");
-                liMoney.className = "data";
-                liMoney.textContent = money;
-                ul.insertAdjacentElement("beforeend", liMoney)
-            };
-            
+
+                list
+                    .appendChild(liCountry)
+                    .appendChild(ul)
+                    .appendChild(liCapital)
+                    .insertAdjacentElement("afterend", liRegion)
+                    .insertAdjacentElement("afterend", liSubRegion);
+
+                for (lang in country.languages) {
+
+                    let languages = `Langue : ${country.languages[lang]}`
+                    let liLanguages = document.createElement("li");
+                    liLanguages.className = "data";
+                    liLanguages.textContent = languages;
+                    ul.insertAdjacentElement("beforeend", liLanguages)
+                };
+
+                for (currency in country.currencies) {
+
+                    let money = `Monaie : ${country.currencies[currency].name} "${country.currencies[currency].symbol}"`
+                    let liMoney = document.createElement("li");
+                    liMoney.className = "data";
+                    liMoney.textContent = money;
+                    ul.insertAdjacentElement("beforeend", liMoney)
+                };
+
+            });
+
         });
-        
-    });
-    
+
 };
 
 btn.addEventListener("click", (getMyCountries));
